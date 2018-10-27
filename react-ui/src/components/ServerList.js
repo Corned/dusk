5bd57d0077262d07f7702a6219951578c9c03f0b
Corned <oliver@kaarnalehto.fi>
2018-10-27 15:12:47 +0300

    Servernames are visible on hover

--- react-ui/src/components/ServerList.js
+++ react-ui/src/components/ServerList.js
@@ -6,18 +6,41 @@ class ServerList extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { }
+    this.state = { servers: [ "Hello World", "React Developers", "r/programminghumor" ] }
   }
 
   render() {
     return (
       <div className="ServerList">
-        <div className="ServerBlob"/>
+        <ServerButton name={"Home"} onClick={() => console.log("home")}/>
         <div className="Separator"/>
-        <div className="ServerBlob"/>
-        <div className="ServerBlob"/>
+        { this.state.servers.map(name => <ServerButton name={name}/>)}
         <div className="CreateServerBlob"/>
         <LogoutButton/>
+      </div>
+    );
+  }
+}
+
+class ServerButton extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { displayServerName: false }
+  }
+
+  onMouseEnter = () => {
+    this.setState({ displayServerName: true })
+  }
+
+  onMouseLeave = () => {
+    this.setState({ displayServerName: false })
+  }
+
+  render() {
+    return (
+      <div className="ServerBlob" onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+        { this.state.displayServerName && <span>{this.props.name}</span>}
       </div>
     );
   }
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 # Dusk
 
-Dusk is a chatting application clone based on a wildly popular chat for gamers. I am developing this for learning purposes only.
+Dusk is a chatting application based on a wildly popular chat for gamers. I am developing this for learning purposes only. You could call Dusk a clone.
+
+## Setting Up
+
+1. `git clone git@github.com:Corned/dusk.git`
+2. Run `npm install` in both `./` and `./react-ui`
+3. Create `.env` file in `./` and define `PRODUCTION_DATABASE_URL` and `MY_SPECIAL_SECRET`
+4. Run `npm run dev` in `./`. This will start both the server and the client.
+
+## Production Build
+
+I will be wiping the database once in a while. [Dusk](http://dusk-.herokuapp.com).
